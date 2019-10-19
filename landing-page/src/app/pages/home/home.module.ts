21387320e9fbba44e040786daa98bf358c78100f
Julio Adán Pérez García <julioperezag@gmail.com>
2019-10-18 21:54:59 -0500

    Add home image
    * Add parallax

--- landing-page/src/app/pages/home/home.module.ts
+++ landing-page/src/app/pages/home/home.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ParallaxModule } from 'ngx-parallax';
 
 const routes: Routes = [
     {
-        path: "",
+        path: '',
         component: HomeComponent
     }
-]
+];
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, RouterModule.forChild(routes)],
+    imports: [CommonModule, RouterModule.forChild(routes), ParallaxModule],
     exports: [HomeComponent]
 })
 
-export class HomeModule { }
+export class HomeModule {
+}
